41191ec166141280dafbafa6c8f9d3c2a45aa149
Kis123mas <emmanuelezekielsolomon@gmail.com>
2022-06-07 12:30:42 -0700

    function that checks for alphabetic character.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * _isalpha - Shows 1 if the input is a
  * let Another cases, shows 0
  *
- * c: The character in ASCII code
+ * @c: The character in ASCII code
  *
  * Return: 1 for letters. 0 for the rest.
  */
