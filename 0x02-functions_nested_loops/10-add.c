a136fb813704bc75551145aa66b7adf0672d8e3a
Kis123mas <emmanuelezekielsolomon@gmail.com>
2022-06-07 13:39:10 -0700

    function that adds two integers and returns the result.

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,8 +3,8 @@
 /**
  * add - Adds two integers and returns the result
  *
- * a: number one.
- * b: number two.
+ * @a: number one.
+ * @b: number two.
  *
  * Return: Add of number one and number two.
  */
