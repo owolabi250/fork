0c4a49217ec671f3b052ed7f5727f47eb1e94651
Kis123mas <emmanuelezekielsolomon@gmail.com>
2022-06-07 12:46:53 -0700

    function that prints the sign of a number.

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -4,7 +4,7 @@
  * print_sign - Determines if the input number
  * greater, equal or less than zero.
  *
- * n: The input number as an integer.
+ * @n: The input number as an integer.
  * Return: 1 is greater than zero. 0 is zero.
  * -1 is less than zero.
  */
