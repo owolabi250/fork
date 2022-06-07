21988ac681fd3faa5c10857f6ac68cde82915fbe
Kis123mas <emmanuelezekielsolomon@gmail.com>
2022-06-07 05:51:50 -0700

    Task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,6 @@
-#ifndef main_h_
-#define _main_h_
-
-int _putchar(char);
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -14,5 +13,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
+void print_times_table(int n);
 #endif
