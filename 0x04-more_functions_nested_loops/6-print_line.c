ba978fee32db5d6fe6fb86407834db49ef119cb2
Kis123mas <emmanuelezekielsolomon@gmail.com>
2022-06-09 13:02:20 -0700

    function that draws a straight line in the terminal.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints straight line
+ *
+ * @n: returned integer
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
